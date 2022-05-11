a23bf58034a273608a4987802f771026b6c7fdfa
aisakuulu <a.isakuulu@gmail.com>
2022-05-11 14:51:14 +0600

    I put some changes

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -4,11 +4,40 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+
+        System.out.println();
+
+        Cat catOne = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Name : " + catOne.getName());
+        System.out.println("Color : " + catOne.getColor());
+
+        System.out.println();
+
+        Cat catTwo = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Name : " + catTwo.getName());
+        System.out.println("Color : " + catTwo.getColor());
+
+        System.out.println();
+
+        Boolean compare = catOne.equals(catTwo);
+        System.out.println("Comparing two Cat Beans : " + compare);
+
+        System.out.println(catOne);
+        System.out.println(catTwo);
+
     }
 }
--- src/test/java/peaksoft/AppTest.java
+++ src/test/java/peaksoft/AppTest.java
@@ -1,5 +1,5 @@
-package peaksoft;
-
+//package peaksoft;
+//
 //import org.junit.Assert;
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
